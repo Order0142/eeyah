9b88f63120243bdffeab8d51f3e81ec687314855
Ade2002 <sikirusulaimon2002@gmail.com>
2023-02-13 10:59:24 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
 const Home = () => {
   return (
-  <>
-  <Header ColorFill={'white'} colors='white'/>
-    <main>
-<section className={'section'} id={'hero'} ></section>
-    </main>
-    <button>Ade</button>
-  </>
-  )
-}
+    <>
+      <Header ColorFill={"white"} colors="white" />
+      <main>
+        <section className={"section"} id={"hero"}>
+          <div>
+            <h2>
+              We help you grow your business{" "}
+              <span>
+                <b>faster</b> <img src={""} alt={""} />
+              </span>
+            </h2>
+            <p>
+              Ehya is the Instagram analytics platform teams use to stay focused
+              on the goals, track engagement for report your business .
+            </p>
+            <button>See how it works</button>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
