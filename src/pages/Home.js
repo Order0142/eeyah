f748bb8ae7e1fc74ca2a1360e06829c1bba07549
Ade2002 <sikirusulaimon2002@gmail.com>
2023-02-13 11:57:10 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 import Images from "../images/Images";
 const Home = () => {
   return (
-      <main>
-        <Header ColorFill={"white"} colors="white" />
-        <section className={"section"} id={"hero"}>
-          <div>
-            <h2>
-              We help you grow your business <span>faster</span>
-            </h2>
-            <p>
-              Ehya is the Instagram analytics platform teams use to stay focused
-              on the goals, track engagement for report your business .
-            </p>
-            <span>
-              <Link to={"/how"} className="linkBtn">
-                See how it works
-              </Link>
-              <Link to={"/how"} className="">
-                Get a free demo →{" "}
-              </Link>
-            </span>
-          </div>
-          <img src={Images.HeroImg} alt={"heroImg"} />
-        </section>
-      </main>
+    <main>
+      <Header ColorFill={"white"} colors="white" />
+      <section className={"section"} id={"hero"}>
+        <div>
+          <h2>
+            We help you grow your business <span>faster</span>
+          </h2>
+          <p>
+            Ehya is the Instagram analytics platform teams use to stay focused
+            on the goals, track engagement for report your business .
+          </p>
+          <span>
+            <Link to={"/how"} className="linkBtn">
+              See how it works
+            </Link>
+            <Link to={"/how"} className="">
+              Get a free demo →{" "}
+            </Link>
+          </span>
+        </div>
+        <img src={Images.HeroImg} alt={"heroImg"} />
+      </section>
+    </main>
   );
 };
 
