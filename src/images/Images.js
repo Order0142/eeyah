2e549ca20d6586d29d0954bac3e23ec8b79349bf
Ade2002 <sikirusulaimon2002@gmail.com>
2023-02-16 11:40:55 +0100

    update

--- src/images/Images.js
+++ src/images/Images.js
@@ -1,9 +1,19 @@
-import Logo from './Logo.svg'
-import HeroImg from './heroImg.png'
-import BorderImg from './borderImg.svg'
+import Logo from "./Logo.svg";
+import HeroImg from "./heroImg.png";
+import Slack from "./slack.png";
+import Uber from "./uber.png";
+import Netflix from "./netflix.png";
+import Google from "./google.png";
+import Airbnb from "./airbnb.png";
+import BorderImg from "./borderImg.svg";
 const Images = {
-Logo,
-HeroImg,
-BorderImg,
-}
+  Logo,
+  HeroImg,
+  BorderImg,
+  Airbnb,
+  Netflix,
+  Slack,
+  Google,
+  Uber,
+};
 export default Images;
